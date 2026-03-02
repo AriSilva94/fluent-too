5707e7d0d9e723cc9def0fe72f94579bbe03afa7
AriSilva94 <ariovaldo.bsjunior@gmail.com>
2026-03-01 21:48:30 -0300

    feat: create opengraph-image

---
+++ app/[locale]/twitter-image.tsx
@@ -0,0 +1,16 @@
+import type { Locale } from "@/lib/i18n";
+import { createSocialImage, socialImageContentType, socialImageSize } from "@/lib/socialImage";
+
+export const size = socialImageSize;
+export const contentType = socialImageContentType;
+export const alt = "Fluent Too social preview";
+
+export default async function TwitterImage({
+  params,
+}: {
+  params: Promise<{ locale: Locale }>;
+}) {
+  const { locale } = await params;
+
+  return await createSocialImage(locale);
+}
